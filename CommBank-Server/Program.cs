a19c4749c9cc7f44c775ffde4270d7963139b6a6
peach003 <8600315@gmail.com>
2025-04-03 12:33:21 +1300

    Support icons

--- CommBank-Server/Program.cs
+++ CommBank-Server/Program.cs
@@ -5,19 +5,17 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Ensure Secrets.json is loaded
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("Secrets.json", optional: true, reloadOnChange: true);
 
-// Get MongoDB connection string from Secrets.json
 var mongoClient = new MongoClient(builder.Configuration.GetConnectionString("CommBank"));
 var mongoDatabase = mongoClient.GetDatabase("CommBank");
 
-// Register services
+
 builder.Services.AddSingleton<IAccountsService>(new AccountsService(mongoDatabase));
 builder.Services.AddSingleton<IAuthService>(new AuthService(mongoDatabase));
 builder.Services.AddSingleton<IGoalsService>(new GoalsService(mongoDatabase));
@@ -25,7 +23,6 @@ builder.Services.AddSingleton<ITagsService>(new TagsService(mongoDatabase));
 builder.Services.AddSingleton<ITransactionsService>(new TransactionsService(mongoDatabase));
 builder.Services.AddSingleton<IUsersService>(new UsersService(mongoDatabase));
 
-// Enable CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins", builder =>
@@ -38,33 +35,33 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// CORS setup to allow all origins, methods, and headers
+
 app.UseCors("AllowAllOrigins");
 
-// Configure Swagger for development environment
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Enable HTTPS redirection
+
 app.UseHttpsRedirection();
 
-// Enable authorization middleware
+
 app.UseAuthorization();
 
-// Map controllers to the app
+
 app.MapControllers();
 
-// Log request details for debugging
+
 app.Use(async (context, next) =>
 {
-    // Log request method and URL
+    
     Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
     await next.Invoke();
 });
 
-// Run the application
+
 app.Run();
 
--- CommBank-Server/obj/Debug/net8.0/CommBank.AssemblyInfo.cs
+++ CommBank-Server/obj/Debug/net8.0/CommBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0daa1cb7afdf2781dd4c7daf00ffaa9ed2d5a091")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
